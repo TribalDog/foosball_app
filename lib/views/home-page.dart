ba6550f2b53c49d1f91678daccb43f80b6d71528
Raoul Mackle <raoul@clevermedkits.com>
2022-05-15 14:58:46 +1200

    Refactored NFC tag reader. Added Android intent handler.

--- lib/views/home-page.dart
+++ lib/views/home-page.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:nfc_manager/nfc_manager.dart';
+import 'package:receive_intent/receive_intent.dart' as AndroidIntent;
 
 class HomePage extends StatefulWidget {
   HomePage({Key? key, required this.title}) : super(key: key);
@@ -15,11 +16,44 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   String _sideId = '';
+  String _errorMessage = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _init();
+  }
+
+  Future<void> _init() async {
+    final receivedIntent = await AndroidIntent.ReceiveIntent.getInitialIntent();
+
+    if (!mounted) return;
+
+    if (receivedIntent!.data != null) {
+      _setSideState(receivedIntent.data);
+    }
+  }
+
+  void _setSideState(String? uri) {
+    String sideId = '';
+
+    if ((uri != null) && (uri.contains('red'))) {
+      sideId = 'Red';
+    }
+    if ((uri != null) && (uri.contains('blue'))) {
+      sideId = 'Blue';
+    }
+
+    setState(() {
+      _sideId = sideId;
+    });
+  }
 
   void _scan() async {
     String sideId = '';
     setState(() {
       _sideId = sideId;
+      _errorMessage = '';
     });
 
     bool isAvailable = await NfcManager.instance.isAvailable();
@@ -29,27 +63,26 @@ class _HomePageState extends State<HomePage> {
         onDiscovered: (NfcTag tag) async {
           Ndef? ndef = Ndef.from(tag);
           if (ndef == null) {
-            print('Tag is not compatible with NDEF');
+            setState(() {
+              _errorMessage = 'This is not a valid Foosball side tag.';
+            });
             return;
           }
 
+          bool found = false;
           NdefMessage ndefMessage = await ndef.read();
           ndefMessage.records.forEach((record) {
             if (record.typeNameFormat == NdefTypeNameFormat.nfcWellknown) {
-              var rawSideId = utf8.decode(record.payload);
-
-              if (rawSideId.contains('SideA')) {
-                sideId = 'Red';
-              }
-              if (rawSideId.contains('SideB')) {
-                sideId = 'Blue';
-              }
+              found = true;
+              _setSideState(utf8.decode(record.payload));
             }
           });
 
-          setState(() {
-            _sideId = sideId;
-          });
+          if (!found) {
+            setState(() {
+              _errorMessage = 'This is an unknown Foosball side tag.';
+            });
+          }
 
           await Future.delayed(const Duration(seconds: 2), () {
             NfcManager.instance.stopSession();
@@ -74,15 +107,30 @@ class _HomePageState extends State<HomePage> {
             ),
             Text(
               '$_sideId',
-              style: Theme.of(context).textTheme.headline4,
+              style: TextStyle (
+                  fontSize: 48,
+                  fontWeight: FontWeight.bold,
+                  color: (_sideId == 'Red') ? Colors.red:Colors.blue
+              ),
+            ),
+            SizedBox(
+              height: 48,
+            ),
+            Text(
+             '$_errorMessage',
+              style: TextStyle (
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: Colors.red
+              ),
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _scan,
-        tooltip: 'Scan',
-        child: Icon(Icons.add),
+        tooltip: 'Re-Scan',
+        child: Icon(Icons.nfc),
       ),
     );
   }
